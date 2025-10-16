bc56c681bb8c5932fdaeee55caa0000c1f043f32
Ashwin-k-05 <ashwinn.k2005@gmail.com>
2025-10-16 10:29:13 +0530

    REACT ROOTS

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import User from "./components/User"
+import Home from "./Pages/Home"
+import Contact from "./Pages/Contact"
+import Service from "./Pages/Service"
+import About from "./Pages/About"
+import {Route,Routes} from 'react-router-dom'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Routes>
+    <Route path="/" Component={Home}> </Route>
+    <Route path="/about" Component={About}> </Route>
+    <Route path="/contact" Component={Contact}> </Route>
+     <Route path="/service" Component={Service}> </Route>
+    </Routes>
+    </>
   )
 }
 
