9833cb2632d0e73f20e70409eb93c4d6f12861f5
Ashwin-k-05 <ashwinn.k2005@gmail.com>
2026-01-21 20:39:31 +0530

    Delete routes directory

--- routes/taskroutes.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {createTask} = require('../controllers/taskcontrollers');     
-router.post('/createtask',createTask);
-module.exports = router;
